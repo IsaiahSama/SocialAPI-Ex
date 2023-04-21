dd30085e2f620468231c01ee42bbff00837a8ed7
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-21 13:36:32 -0400

    Added skeleton routes for User

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,40 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("Oh wow, the user page, nothing to see here!");
+});
+
+// Getters
+
+router.get("/view/:userID/", (req, res) => {
+  userID = req.params.userID;
+  res.send("Displaying information for user with ID of: " + userID);
+});
+
+router.get("/update/", (req, res) => {
+  res.render("profile");
+});
+
+router.get("/getposts/:userID/", (req, res) => {
+  res.send(
+    "Some JSON object containting all posts by a specified user with id: " +
+      req.params.userID
+  );
+});
+
+// Posters
+router.post("/create/", (req, res, next) => {
+  console.log("Creates user in here.");
+});
+
+// Putters
+router.put("/update/", (req, res) => {
+  console.log("Update user information here.");
+});
+
+// Deleters
+router.delete("/delete/", (req, res) => {
+  console.log("Delete this!");
 });
 
 module.exports = router;
