3cc0c5c8649525450ef66b024e1d244c04bae9d4
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-21 13:42:52 -0400

    Added routes for Posts

--- routes/post.js
+++ routes/post.js
@@ -5,4 +5,32 @@ router.get("/", (req, res) => {
   res.render("post");
 });
 
+// Getters
+router.get("/create/", (req, res) => {
+  res.send("Beep Boop. Page for you to create a post here!");
+});
+
+router.get("/view/:postID/", (req, res) => {
+  res.send("Oh look, here's a post with the id: " + req.params.postID);
+});
+
+router.get("/get/:amount", (req, res) => {
+  res.send("Here you go! " + req.params.amount + " posts for you!");
+});
+
+// Posters
+router.post("/create/", (req, res) => {
+  console.log("Get the data from the form and post it to the db ig...");
+});
+
+// Putters
+router.put("/update/:postID", (req, res) => {
+  console.log("Updating the post!");
+});
+
+// Deleters
+router.delete("/delete/:postID", (req, res) => {
+  console.log("they who wish to delete is the author of the post!");
+});
+
 module.exports = router;
