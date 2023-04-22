5987ed0b8c47449a5784b1afcc348a980b2d043f
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-22 11:11:49 -0400

    Setup for register component

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+// Getters
 router.get("/", (req, res) => {
   res.render("login");
+});
+
+router.get("/register/", (req, res) => {
+  res.render("register");
+});
+
+// Posters
+router.post("/", (req, res) => {
+  console.log("Will log in the user!");
+  res.redirect("../");
 });
 
 module.exports = router;
